86b673f0d85766fad45969e2bc410774917651c7
Mircea-George Cozarev <mircea2501@gmail.com>
2023-05-04 20:39:51 +0300

    Add and configure Rubocop

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
+  post 'planifications/initialize_scheduling_session'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
