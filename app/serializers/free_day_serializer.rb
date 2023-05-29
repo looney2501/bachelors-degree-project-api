bb35b35c0772b1178d1d8c60cf6e43a38f979bbf
Mircea-George Cozarev <mircea2501@gmail.com>
2023-05-29 09:52:16 +0300

    Add schema info for several serializers

--- app/serializers/free_day_serializer.rb
+++ app/serializers/free_day_serializer.rb
@@ -3,3 +3,20 @@
 class FreeDaySerializer < RootSerializer
   attributes :id, :date, :free_day_type
 end
+
+# == Schema Information
+#
+# Table name: free_days
+#
+#  id                       :bigint           not null, primary key
+#  date                     :date
+#  free_day_type            :string
+#  free_days_container_type :string           not null
+#  created_at               :datetime         not null
+#  updated_at               :datetime         not null
+#  free_days_container_id   :bigint           not null
+#
+# Indexes
+#
+#  index_free_days_on_free_days_container  (free_days_container_type,free_days_container_id)
+#
--- app/serializers/vacation_serializer.rb
+++ app/serializers/vacation_serializer.rb
@@ -11,3 +11,20 @@ class VacationSerializer < RootSerializer
     object.free_days.map(&:date)
   end
 end
+
+# == Schema Information
+#
+# Table name: vacations
+#
+#  id                  :bigint           not null, primary key
+#  created_at          :datetime         not null
+#  updated_at          :datetime         not null
+#  planning_session_id :bigint
+#  user_id             :bigint
+#
+# Indexes
+#
+#  index_vacations_on_planning_session_id              (planning_session_id)
+#  index_vacations_on_user_id                          (user_id)
+#  index_vacations_on_user_id_and_planning_session_id  (user_id,planning_session_id) UNIQUE
+#
