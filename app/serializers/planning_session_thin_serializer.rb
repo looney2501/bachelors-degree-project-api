a9bec9554b881f71fc50ad82a82aa34400d82ac4
Mircea-George Cozarev <mircea2501@gmail.com>
2023-06-04 10:14:27 +0300

    Add attribute to serializer

--- app/serializers/planning_session_thin_serializer.rb
+++ app/serializers/planning_session_thin_serializer.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class PlanningSessionThinSerializer < RootSerializer
-  attributes :id, :national_free_days, :year, :status
+  attributes :id, :national_free_days, :year, :status, :available_free_days
 
   def national_free_days
     serialize(object.national_free_days, each_serializer: FreeDaySerializer)
